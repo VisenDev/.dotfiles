19eb2f3211e9cd5b72a160e4bc3a07825de83c56
Robert Burnett <dr.slate2@gmail.com>
2024-12-30 12:32:44 -0600

    nvim and emacs changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,12 +15,12 @@ vim.cmd([[
 	set hidden
 
     "fix tabs
-	set tabstop=4
-	set shiftwidth=4
+	set tabstop=2
+	set shiftwidth=2
 	set expandtab
 
     "disable parenthesis matching
-    let g:loaded_matchparen=1
+    "let g:loaded_matchparen=1
 
     "better escape
     imap jk <Esc>
@@ -56,49 +56,14 @@ vim.cmd([[
     colorscheme slate
 
     set formatoptions-=cro
+
+    "warn on column 80
+    if exists('+colorcolumn')
+      set colorcolumn=80
+    else
+      au BufWinEnter * let w:m2=matchadd('ErrorMsg', '\%>80v.\+', -1)
+    endif
 ]])
-
-local function show_floating_notification(message)
-  local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-  local width = 30 
-  local height = 1
-  
-  -- Set the buffer contents
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { message })
-  
-  -- Define floating window options
-  local opts = {
-    relative = 'editor',
-    width = width,
-    height = height,
-    col = 1,--(vim.o.columns - width) / 2,
-    row = (vim.o.lines - height) / 2,
-    anchor = 'NW',
-    border = 'rounded',
-    style = 'minimal',
-  }
-
-  -- Create a floating window
-  local win = vim.api.nvim_open_win(buf, true, opts)
-
-  -- Auto-close the floating window after 3 seconds
-  vim.defer_fn(function()
-    -- Check if the window is still open before attempting to close it
-    if vim.api.nvim_win_is_valid(win) then
-      vim.api.nvim_win_close(win, true)
-    end
-  end, 350)
-end
-
---Quick buffer swap
---vim.api.nvim_create_autocmd('BufWinEnter', {
---    pattern = '',
---    callback = function()
---        local buf = vim.api.nvim_get_current_buf()  -- Get the current buffer ID
---        local buf_name = vim.api.nvim_buf_get_name(buf)  -- Get the buffer name
---        show_floating_notification(buf_name)
---    end,
---})
 
 vim.api.nvim_set_keymap('n', '<Space>', ':bn<Cr>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<S-Space>', ':bp<Cr>', {noremap=true})
@@ -153,6 +118,17 @@ vim.api.nvim_create_autocmd('FileType', {
     end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'c',
+    callback = function(ev)
+        vim.lsp.start({
+            name = 'clangd',
+            cmd = {'clangd'},
+            --root_dir = vim.fs.dirname(vim.fs.find({'build.zig'}, { upward = true })[1]),
+        })
+    end,
+})
+
 --require'lspconfig'.lua_ls.setup{}
 
 --vim.api.nvim_create_autocmd('FileType', {
@@ -200,10 +176,3 @@ vim.api.nvim_create_autocmd('FileType', {
         })
     end,
 })
-
---vim.api.nvim_create_autocmd('BufWritePost', {
-    --    pattern = '*',
-    --    callback = function(ev)
-        --        vim.cmd.lclose()
-        --    end,
-        --})

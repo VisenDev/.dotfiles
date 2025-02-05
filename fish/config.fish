d1ed09574c1b683e62635bec3ec748c7b0f1fb19
Robert Burnett <dr.slate2@gmail.com>
2025-02-05 10:11:51 -0600

    updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,9 @@ end
 setfont /usr/share/kbd/consolefonts/ter-928b.psf.gz
 
 set i2p_browser "chromium --proxy-server=\"http://127.0.0.1:4444\""
+#set GHOSTTY_RESOURCES_DIR ~/zig/ghostty/zig-out/share/ghostty/
+set XDG_CURRENT_DESKTOP Hyprland
+
+function add_cwd_to_path --on-variable PWD
+    set -g fish_user_paths $PWD $fish_user_paths
+end
